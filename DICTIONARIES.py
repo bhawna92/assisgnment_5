160adddf31a0ec68e662a2a9323bfc0753dbfc30
bhawna92 <bhawnadh92@gmail.com>
2025-06-09 23:25:11 +0530

    Add files via upload

---
+++ DICTIONARIES.py
@@ -0,0 +1,38 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "53b0b9eb-93d1-4f21-a194-fdc30f2e4856",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#dictionaries\n",
+    "dict1={'alice':85,'john':65,'cena':90}\n",
+    "name = input(\"Enter the student name: \")\n",
+    "print(dict1.get(name,\"not found\"))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ list.py
@@ -0,0 +1,5 @@
+#LIST
+list1=[1,2,3,4,5,6,7,8,9,10]
+print("original list:",list1)
+print("extracted first 5 items:",list1[0:5])
+print("reversed first 5 items:",list(reversed(list1[0:5])))
